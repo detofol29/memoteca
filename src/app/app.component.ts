eceecb0d83dd5ec91e15a924882ba12a64d5b497
Gabriel Detofol <detofol29@gmail.com>
2025-02-18 20:49:57 -0300

    Adicionando arquivos primeito commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { CabecalhoComponent } from "./componentes/cabecalho/cabecalho.component";
+import { RodapeComponent } from './componentes/rodape/rodape.component';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, CabecalhoComponent, RodapeComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'memoteca';
+}

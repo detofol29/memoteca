53502bfc4a33f5fb487eb7958b848702b6452f52
Gabriel Detofol <detofol29@gmail.com>
2025-02-22 07:44:05 -0300

    Aplicando ngModel

--- src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
+++ src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -1,11 +1,25 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-criar-pensamento',
-  imports: [],
+  imports: [
+    FormsModule],
+  standalone: true, // Indica que é um Standalone Component
   templateUrl: './criar-pensamento.component.html',
   styleUrl: './criar-pensamento.component.css'
 })
 export class CriarPensamentoComponent {
 
+    pensamento = {
+      id: '1',
+      conteudo: 'Aprendendo Angular',
+      autoria: 'Dev',
+      modelo: ''
+    }
+
+    criarPensamento(){
+      alert("Pensamento criado com sucesso!");
+    }
 }
